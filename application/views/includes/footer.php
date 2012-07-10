58850188f8e6e068789bf5c4a4f3c8b5cbb91132
Amar H <hajrovica@gmail.com>
2012-07-10 10:34:50 +0200

    Template and TwitterBootstrap
    Added twitter bootstrap files, made simple termplate solution with
    costum _outpt funct, included bootstrap and Jquery, Some modifications
    for autoloada, DB, RB and URL

---
+++ application/views/includes/footer.php
@@ -0,0 +1,9 @@
+</div> <!-- /container -->
+<!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.js"></script>
+    <script src="<?php echo base_url('assets/tb/js/bootstrap.js'); ?>"></script>
+
+  </body>
+</html>
---
+++ application/views/template.php
@@ -0,0 +1,12 @@
+<?php
+
+//load header
+$this->load->view('includes/header');
+
+//load main content
+$this->load->view($main_content);
+
+//load footer
+$this->load->view('includes/footer');
+
+
